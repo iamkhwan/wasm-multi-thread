a0ea6dabcb31558ccf836467616332998df9d0e4
Puttarak Boonyatham <puttarakboonyatham@Puttaraks-MacBook-Pro.local>
2023-01-03 14:39:24 +0700

    init wasm app

---
+++ WasmMultithreading/main.js
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './dotnet.js'
+
+const { getAssemblyExports, getConfig } = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+const config = getConfig();
+
+document.getElementById('out').innerHTML = `WASM experimental, Look in the console !!!`;
+await dotnet.run();
+
